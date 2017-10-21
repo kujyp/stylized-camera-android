875b8c85a6d96bb707becbc3e1dcb92aefaf439d
pjo901018 <pjo901018@korea.ac.kr>
2017-10-21 19:01:49 +0900

    Adding stylename

--- app/src/main/java/com/asuscomm/yangyinetwork/stylizedcamera/Task.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/stylizedcamera/Task.java
@@ -1,24 +1,65 @@
 package com.asuscomm.yangyinetwork.stylizedcamera;
 
+import com.google.firebase.database.ServerValue;
+
 /**
  * Created by jaeyoung on 14/10/2017.
  */
 
 public class Task {
-    private String uri;
+    private String downloadUrl;
+    private String path;
+    private String styleName;
+    private Object createdAt;
 
     public Task() {
+        this(null, null);
     }
 
-    public Task(String path) {
-        uri = path;
+    public Task(String downloadUrl, String path) {
+        this(downloadUrl, path, null);
     }
 
-    public String getUri() {
-        return uri;
+    public Task(String downloadUrl, String path, String styleName) {
+        this(downloadUrl, path, styleName, ServerValue.TIMESTAMP);
     }
 
-    public void setUri(String uri) {
-        this.uri = uri;
+    public Task(String downloadUrl, String path, String styleName, Object createdAt) {
+        this.downloadUrl = downloadUrl;
+        this.path = path;
+        this.styleName = styleName;
+        this.createdAt = createdAt;
+    }
+
+    public String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getStyleName() {
+        return styleName;
+    }
+
+    public void setStyleName(String styleName) {
+        this.styleName = styleName;
+    }
+
+    public Object getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Object createdAt) {
+        this.createdAt = createdAt;
     }
 }
--- app/src/main/java/com/asuscomm/yangyinetwork/stylizedcamera/ViewImageActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/stylizedcamera/ViewImageActivity.java
@@ -1,7 +1,6 @@
 package com.asuscomm.yangyinetwork.stylizedcamera;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,16 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
-
-import java.io.ByteArrayOutputStream;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +36,7 @@ public class ViewImageActivity extends AppCompatActivity {
 
     @BindView(R.id.imageView)
     ImageView mImageView;
+    private String styleName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +55,17 @@ public class ViewImageActivity extends AppCompatActivity {
     private void saveOnFbStorage(Uri imageUri) {
         FirebaseStorage storage = FirebaseStorage.getInstance("gs://styletransfer-ba06f.appspot.com");
         StorageReference storageRef = storage.getReference();
-        String filename = "a.jpg";
+        String filename = imageUri.getLastPathSegment();
         StorageReference fileRef = storageRef.child("upload_images/" + filename);
+
+//        com.google.android.gms.tasks.Task<AuthResult> authResultTask = FirebaseAuth.getInstance().signInAnonymously();
+//        authResultTask.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
+//                Log.d(TAG, "onComplete: ");
+//            }
+//        });
+
         UploadTask uploadTask = fileRef.putFile(imageUri);
 
         uploadTask.addOnFailureListener(new OnFailureListener() {
@@ -64,20 +77,61 @@ public class ViewImageActivity extends AppCompatActivity {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                String path = taskSnapshot.getMetadata().getPath();
                 Uri downloadUrl = taskSnapshot.getDownloadUrl();
                 Log.d(TAG, "onSuccess() called with: downloadUrl = [" + downloadUrl.toString() + "]");
 
-                saveOnFbDatabase(downloadUrl);
+                saveOnFbDatabase(downloadUrl, path);
             }
         });
     }
 
-    private void saveOnFbDatabase(Uri downloadUrl) {
+    private void saveOnFbDatabase(final Uri downloadUrl, final String path) {
         Log.d(TAG, "saveOnFbDatabase: ");
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("uploaded_tasks");
+        final DatabaseReference myRef = database.getReference("uploaded_tasks");
 
-        Task task = new Task(downloadUrl.toString());
-        myRef.push().setValue(task);
+        getStyleName(new OnFinishListener() {
+            @Override
+            public void onFinish(String style) {
+                Task task = new Task(downloadUrl.toString(), path, style);
+                myRef.push().setValue(task);
+            }
+        });
+    }
+
+    interface OnFinishListener {
+        void onFinish(String style);
+    }
+
+    public String getStyleName(final OnFinishListener finished) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("styles");
+        myRef.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (int idx = 0; idx < dataSnapshot.getChildrenCount(); idx++) {
+                            DataSnapshot item = dataSnapshot.getChildren().iterator().next();
+                            if (idx == 0) {
+                                item.getKey();
+                            }
+                        }
+                        for (DataSnapshot item:
+                                dataSnapshot.getChildren()) {
+                            finished.onFinish(item.getKey());
+                            break;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                }
+        );
+
+        return styleName;
     }
 }
